0f5484cacb7cc9dfc1f097188ee346e284276e31
Rakesh Kumar <113333863+Rakesh202122@users.noreply.github.com>
2025-03-22 09:40:28 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,10 +4,11 @@ import NextAuth from 'next-auth'
 // import GoogleProvider from 'next-auth/providers/google'
 // import EmailProvider from 'next-auth/providers/email'
 import GitHubProvider from 'next-auth/providers/github'
+import GoogleProvider from "next-auth/providers/google";
 import mongoose from 'mongoose'
-import connectDB from '@/db/connectDb'
 import User from '@/models/User'
 import Payment from '@/models/Payment'
+import connectDB from '@/db/connectDb'
 
 export const authoptions = NextAuth({
     providers: [
@@ -24,10 +25,10 @@ export const authoptions = NextAuth({
     //     clientId: process.env.FACEBOOK_ID,
     //     clientSecret: process.env.FACEBOOK_SECRET
     //   }),
-    //   GoogleProvider({
-    //     clientId: process.env.GOOGLE_ID,
-    //     clientSecret: process.env.GOOGLE_SECRET
-    //   }),
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET
+      }),
     //   // Passwordless / email sign in
     //   EmailProvider({
     //     server: process.env.MAIL_SERVER,
@@ -37,30 +38,44 @@ export const authoptions = NextAuth({
 
     //use for storing signIn data in database
     callbacks: {
-      async signIn({ user, account, profile, email, credentials }) {
-        if(account.provider == "github"){
-          await connectDB()
-          
-          //check if the user already exists in the DB
-          const currentUser = await User.findOne({ email: email })
-          if (!currentUser) {
+    async signIn({ user, account, profile, email, credentials }) {
+      await connectDB()
+      if(account.provider == 'github'){
 
-            //create a new user
-            const newUser = await User.create({
-              email: user.email,
-              username: user.email.split("@")[0],
-            })
-          }
-          return true
+        // connect to database
+        const currentUser = await User.findOne({email: user.email})
+
+        if(!currentUser){
+          const newUser = await User.create({
+          email: user.email,
+            username: user.email.split('@')[0],  
+            name: user.name    
+          })
         }
-      },
+     
+        return true
+      }
+      if (account.provider === "google") {
+        const currentUser = await User.findOne({email: user.email})
 
-      async session({ session, user, token }) {
-        const dbUser = await User.findOne({email: session.user.email})
-        session.user.name = dbUser.username
-        return session
-      },
-    }
-  })
+        if(!currentUser){
+          const newUser = await User.create({
+          email: user.email,
+            username: user.email.split('@')[0], 
+            name: user.name     
+          })
+        }
+        return profile.email_verified && profile.email.endsWith("@gmail.com")
+      }
+      return true // Do different verification for other providers that don't have `email_verified`
+    
+  },
+  async session({ session, user, token }) {
+    const dbUser = await User.findOne({email: session.user.email})
+    session.user.name = dbUser.username
+    return session
+  },
+}
+})
 
-  export { authoptions as GET, authoptions as POST}
+export { authoptions as GET , authoptions as POST}
